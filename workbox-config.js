ac9664886d0fb53f9e4a5c87ea6049d08324bddf
toluay <ayantundetolu@gmail.com>
2018-07-09 18:37:22 +0100

    new commit

---
+++ workbox-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns":["**/*.css",
+    "index.html",
+    "js/animation.js",
+    "images/home/*.jpg",
+    "images/icon/*.svg",
+    "pages/offline.html",
+    "pages/404.html"],
+  "swDest": "build/sw.js",
+  "swSrc": "src/sw.js",
+  "globIgnores": [
+    "../workbox-config.js"
+  ]
+};
